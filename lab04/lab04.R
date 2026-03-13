0a907945e1da46acece65263110cba0c0127bf1d
augustouucsd <146669536+augustouucsd@users.noreply.github.com>
2026-03-13 12:42:26 -0700

    lab04

---
+++ lab04/lab04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Lab04 R intro 2026"
+#' author: "Minzhou Ou"
+#' date: "Jan 16th, 2026"
+#' ---
+
+x <- seq(1, 50, by = 0.01)
+plot(x)
+plot(x, sin(x), typ = "l", col = "purple", lwd = 3, xlab = "WHAT")
+
+x <- 1:10
+1:10 * 10
